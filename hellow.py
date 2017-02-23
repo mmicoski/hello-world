5a2e7a546ed4ba0a6f0a9affdca83339ceda7141
mmicoski <mmicoski@gmail.com>
2017-02-22 23:16:32 -0300

    Incluindo função helper

--- hellow.py
+++ hellow.py
@@ -1,5 +1,7 @@
 '''Hello World in Python'''
 
+from hellowHelp import helloHelp
 
 if __name__ == '__main__':
     print "Hello World!"
+    print helloHelp.getHelloString()
--- hellowHelp/helloHelp.py
+++ hellowHelp/helloHelp.py
@@ -1,5 +1,8 @@
-'''Hello World in Python'''
+'''Hello World Helper'''
 
+
+def getHelloString():
+    return "Hello World!!"
 
 if __name__ == '__main__':
     print "Hello World!"
